c0262d93bf9c734ea02077166c14ccf6c9210acc
DrScott <dougscott2@gmail.com>
2015-10-15 12:06:10 -0400

    read posts and display replies

--- src/Forum.java
+++ src/Forum.java
@@ -1,14 +1,45 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by DrScott on 10/15/15.
  */
 public class Forum {
     public static void main(String[] args) {
+        ArrayList<Post> posts = new ArrayList();
+        String postContent = readFile("posts.txt");
+        String[] lines = postContent.split("\n"); //  \n means new lines. will split postContent on the new lines for the array list
 
+        for (String line : lines) {
+            String[] columns = line.split("\\|");
+            int replyID = Integer.valueOf(columns[0]);
+            String author = columns[1];
+            String text = columns[2];
+            Post post = new Post(replyID, author, text);
+            posts.add(post);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int postID = -1;
+        while (true) {
+            printPosts(posts, postID);
+            System.out.println("Enter post id to see replies");
+            postID = Integer.valueOf(scanner.nextLine());
+        }
     }
+    static void printPosts(ArrayList<Post> posts, int postID){
+        int id = 0;
+        for (Post post : posts) {
+            if (post.replyID ==postID) {
+                System.out.println(String.format("[%d] %s (%s)", id, post.text, post.author));
+            }
+            id++;
+        }
+    }
+
 
     static String readFile(String fileName) {
         File f = new File(fileName);
@@ -22,7 +53,6 @@ public class Forum {
             return null;
         }
     }
-
     static void writeFile(String fileName, String fileContent){
         File f = new File(fileName);
         try{
---
+++ src/Post.java
@@ -0,0 +1,14 @@
+/**
+ * Created by DrScott on 10/15/15.
+ */
+public class Post {
+    int replyID;
+    String author;
+    String text;
+
+    public Post(int replyID, String author, String text) {
+        this.replyID = replyID;
+        this.author = author;
+        this.text = text;
+    }
+}
